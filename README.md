b412195e057cfb12edbc8ae4889d5860fa71bea2
Andrzej Miazga <andrzej.miazga96@gmail.com>
2018-11-25 15:11:55 +0100

    Wprowadzenie wielowątkowości.

--- README.md
+++ README.md
@@ -8,7 +8,10 @@ Server supports:
 Once client gets IP address of second client it can directly connect to him, and 
 start exchanging messages.
 All massages (server-to-client, client-to-server, client-client) are defined in pwr_msg_proto directory.
-
+<br>
+<br>
+<b>Server port: 8085</b>
+<br><b>Fixed address IP recommended.
 ## pwr_msg_proto directory 
 pwr_msg_proto is git submodule (https://github.com/MichalGrzesiak/pwr_msg_proto)
 that contains proto definitions of sent messages.
@@ -35,3 +38,58 @@ To run
 java -cp target/Chat-1.0-SNAPSHOT.jar pl.Main
 ```
 
+# Communication with server
+Communication objects (from client):
+- register
+```
+PwrMsg.clinet_to_server.newBuilder().setTypeValue(0).setLoginString(login).setPasswordString(password).build();
+```
+- login
+```
+PwrMsg.clinet_to_server.newBuilder().setTypeValue(1).setLoginString(login).setPasswordString(password).build();
+```
+- getIP
+```
+PwrMsg.clinet_to_server.newBuilder().setTypeValue(2).setLoginString(login).build();
+```
+- logout
+```
+PwrMsg.clinet_to_server.newBuilder().setTypeValue(3).setLoginString(login).build();
+```
+
+## Requests
+Before each query the connection should be initialized:
+````
+socket = new Socket("ipAddr", 8085);
+outputStream = new DataOutputStream(socket.getOutputStream());
+inputStream = new DataInputStream(socket.getInputStream());
+````
+After each query the connection should be closed:
+````
+socket.close();
+````
+
+## Sendind data
+````
+byte[] arr = toSend.toByteArray();
+int length = 0;
+
+outputStream.flush();
+outputStream.writeInt(arr.length);
+outputStream.write(arr);
+````
+
+## Receiving data
+````
+length = inputStream.readInt();
+byte[] arr = new byte[length];
+
+inputStream.read(arr, 0, arr.length);
+toGet = PwrMsg.server_to_clinet.parseFrom(arr);
+````
+
+# JAR
+Executable jar and database files (must be in the same folder!): 
+``
+out/artifacts/ChatServer
+``
--- src/main/java/pl/server/TcpServer.java
+++ src/main/java/pl/server/TcpServer.java
@@ -27,107 +27,17 @@ public class TcpServer {
 
    public void listen() throws IOException {
        while (true) {
+           System.out.println("Nasłuchuję...");
            socket = serverSocket.accept();
            inputStream = new DataInputStream(socket.getInputStream());
            outputStream = new DataOutputStream(socket.getOutputStream());
            System.out.println("Połączenie: " + socket.getInetAddress().getHostName());
 
-           threadFunc(inputStream, outputStream, socket);
+           Thread deamon = new Thread(new DeamonThread(inputStream, outputStream, socket, ipMap));
+           deamon.start();
+
        }
    }
-
-    public void threadFunc(final DataInputStream inputStream, final DataOutputStream outputStream, final Socket socket) {
-        new Thread() {
-            @Override
-            public void run() {
-                PwrMsg.server_to_clinet toSend = null;
-                PwrMsg.clinet_to_server toGet;
-
-                try {
-                    DbController dbController = new DbController();
-                    int length = inputStream.readInt();
-                    byte[] message = new byte[length];
-
-                    if (length > 0) {
-                        inputStream.read(message, 0, message.length); // read the message
-                        toGet = PwrMsg.clinet_to_server.parseFrom(message);
-
-                        System.out.println("Parametry zapytania: " + toGet.getTypeValue() + " " + toGet.getPasswordString() + " " + toGet.getLoginString());
-
-                        switch (toGet.getTypeValue()) {
-                            case 0: { //rejestrujemy
-                                boolean dbResponse;
-                                try {
-                                    dbResponse = dbController.register(toGet.getLoginString(), toGet.getPasswordString());
-                                } catch (SQLException e) {
-                                    dbResponse = false;
-                                    System.out.println("Prawdopodobnie użytkownik istnieje. Albo inny problem z bazą xD");
-                                }
-
-                                toSend = PwrMsg.server_to_clinet.newBuilder().setTypeValue(0).setIsSuccesful(dbResponse).build(); //to nie działa
-                            }
-                            break;
-                            case 1: { //logujemy
-                                boolean dbResponse;
-
-                                try {
-                                    dbResponse = dbController.login(toGet.getLoginString(), toGet.getPasswordString());
-                                    synchronized (ipMap) {
-                                        ipMap.put(toGet.getLoginString(), socket.getInetAddress().getHostName());
-                                        System.out.println(ipMap);
-                                    }
-                                } catch (SQLException e) {
-                                    dbResponse = false;
-                                    System.out.println("Jakiś błąd przy logowaniu");
-                                }
-
-                                toSend = PwrMsg.server_to_clinet.newBuilder().setTypeValue(1).setIsSuccesful(dbResponse).build(); // to działa
-                            }
-                            break;
-                            case 2: { //getIp
-                                String ip = null;
-
-                                try {
-                                    synchronized (ipMap) {
-                                        System.out.println("Pobieram IP " + toGet.getLoginString());
-                                        ip = ipMap.get(toGet.getLoginString());
-                                    }
-                                    System.out.println(ip);
-
-                                    toSend = PwrMsg.server_to_clinet.newBuilder().setTypeValue(2).setIsSuccesful(true).setSecondClinetIp(ip).build();
-                                } catch (Exception e) {
-                                    toSend = PwrMsg.server_to_clinet.newBuilder().setTypeValue(2).setIsSuccesful(false).build();
-                                }
-                            }
-                            break;
-                            case 3: { //wyloguj
-                                try {
-                                    synchronized (ipMap) {
-                                        System.out.println("Usuwam IP " + toGet.getLoginString());
-                                        ipMap.remove(toGet.getLoginString());
-                                        System.out.println("Mapa IP: " + ipMap);
-                                    }
-                                    toSend = PwrMsg.server_to_clinet.newBuilder().setTypeValue(3).setIsSuccesful(true).build();
-                                } catch (Exception e) {
-                                    toSend = PwrMsg.server_to_clinet.newBuilder().setTypeValue(3).setIsSuccesful(false).build();
-                                }
-                            }
-                            default:
-                                break;
-                        }
-
-                        byte[] toClientResponse = toSend.toByteArray();
-
-                        outputStream.flush();
-                        outputStream.writeInt(toClientResponse.length);
-                        outputStream.write(toClientResponse);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }.run();
-    }
 
     public void start() {
         try {
@@ -135,5 +45,136 @@ public class TcpServer {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+}
+
+class DeamonThread implements Runnable {
+    DataInputStream inputStream;
+    DataOutputStream outputStream;
+    Map<String, String> ipMap;
+    Socket socket;
+
+    public DeamonThread(DataInputStream inputStream, DataOutputStream outputStream, Socket socket, Map<String, String> ipMap) {
+        this.inputStream = inputStream;
+        this.outputStream = outputStream;
+        this.ipMap = ipMap;
+        this.socket = socket;
+    }
+
+
+    @Override
+    public void run() {
+        PwrMsg.server_to_clinet toSend = null;
+        PwrMsg.clinet_to_server toGet;
+
+        try {
+            DbController dbController = new DbController();
+            int length = inputStream.readInt();
+            byte[] message = new byte[length];
+
+            if (length > 0) {
+                inputStream.read(message, 0, message.length); // read the message
+                toGet = PwrMsg.clinet_to_server.parseFrom(message);
+
+                System.out.println("Parametry zapytania: " + toGet.getTypeValue() + " " + toGet.getPasswordString() + " " + toGet.getLoginString());
+
+                switch (toGet.getTypeValue()) {
+                    case 0: { //rejestrujemy
+                        boolean dbResponse = register(dbController, toGet);
+                        toSend = PwrMsg.server_to_clinet.newBuilder().setTypeValue(0).setIsSuccesful(dbResponse).build();
+                        break;
+                    }
+                    case 1: { //logujemy
+                        boolean dbResponse = login(dbController, toGet);
+                        toSend = PwrMsg.server_to_clinet.newBuilder().setTypeValue(1).setIsSuccesful(dbResponse).build();
+                        break;
+                    }
+                    case 2: { //getIp
+                        toSend = getIP(toGet);
+                        break;
+                    }
+                    case 3: { //wyloguj
+                        toSend = logout(toGet);
+                        break;
+                    }
+                    default: {
+                        break;
+                    }
+                }
+
+                byte[] toClientResponse = toSend.toByteArray();
+
+                outputStream.flush();
+                outputStream.writeInt(toClientResponse.length);
+                outputStream.write(toClientResponse);
+            }
+        } catch (Exception e) {
+            System.out.println("Rozłączono klienta");
+        }
+    }
+
+    private boolean register(DbController dbController, PwrMsg.clinet_to_server toGet) {
+        boolean dbResponse;
+        try {
+            dbResponse = dbController.register(toGet.getLoginString(), toGet.getPasswordString());
+        } catch (SQLException e) {
+            dbResponse = false;
+            System.out.println("Prawdopodobnie użytkownik istnieje. Albo inny problem z bazą xD");
+        }
+
+        return dbResponse;
+    }
+
+    private boolean login(DbController dbController, PwrMsg.clinet_to_server toGet) {
+        boolean dbResponse;
+
+        try {
+            dbResponse = dbController.login(toGet.getLoginString(), toGet.getPasswordString());
+            synchronized (ipMap) {
+                ipMap.put(toGet.getLoginString(), socket.getInetAddress().getHostName());
+                System.out.println("Mapa IP: " + ipMap);
+            }
+        } catch (SQLException e) {
+            dbResponse = false;
+            System.out.println("Jakiś błąd przy logowaniu");
+        }
+
+        return dbResponse;
+    }
+
+    private PwrMsg.server_to_clinet getIP(PwrMsg.clinet_to_server toGet) {
+        PwrMsg.server_to_clinet toSend = null;
+        String ip = null;
+
+        try {
+            synchronized (ipMap) {
+                System.out.println("Pobieram IP " + toGet.getLoginString());
+                ip = ipMap.get(toGet.getLoginString());
+            }
+            System.out.println(ip);
+
+            toSend = PwrMsg.server_to_clinet.newBuilder().setTypeValue(2).setIsSuccesful(true).setSecondClinetIp(ip).build();
+        } catch (Exception e) {
+            toSend = PwrMsg.server_to_clinet.newBuilder().setTypeValue(2).setIsSuccesful(false).build();
+        }
+
+        return toSend;
+    }
+
+    private PwrMsg.server_to_clinet logout(PwrMsg.clinet_to_server toGet) {
+        PwrMsg.server_to_clinet toSend = null;
+
+        try {
+            synchronized (ipMap) {
+                System.out.println("Usuwam IP " + toGet.getLoginString());
+                ipMap.remove(toGet.getLoginString());
+                System.out.println("Mapa IP: " + ipMap);
+            }
+            toSend = PwrMsg.server_to_clinet.newBuilder().setTypeValue(3).setIsSuccesful(true).build();
+        } catch (Exception e) {
+            toSend = PwrMsg.server_to_clinet.newBuilder().setTypeValue(3).setIsSuccesful(false).build();
+        }
+
+        return toSend;
     }
 }
